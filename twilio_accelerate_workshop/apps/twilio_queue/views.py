cf6f5931d342a3cc6b61559b058fff29848301ed
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-07-23 13:39:15 -0400

    Added Twilio Wrapper

--- twilio_accelerate_workshop/apps/twilio_queue/views.py
+++ twilio_accelerate_workshop/apps/twilio_queue/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import resolve_url
-from django.views.decorators.csrf import csrf_exempt
 from twilio import twiml
 from .hold import HoldMusic
+from twilio_accelerate_workshop.wrappers import twilio
 
-@csrf_exempt
+@twilio
 def add_to_queue(request):
 
     r = twiml.Response()
@@ -12,9 +11,9 @@ def add_to_queue(request):
     r.say('Welcome to Acme Industries. To change your hold music dial numbers 1 through 6.')
     r.enqueue('DefaultQueue', waitUrl=resolve_url('queue_wait'))
 
-    return HttpResponse(str(r))
+    return r
 
-@csrf_exempt
+@twilio
 def queue_wait(request, digits=None, music_type=HoldMusic.CLASSICAL, hold_music=HoldMusic):
 
     # Set Music URL
@@ -36,22 +35,22 @@ def queue_wait(request, digits=None, music_type=HoldMusic.CLASSICAL, hold_music=
 
     r.redirect(redirect_url)
 
-    return HttpResponse(str(r))
+    return r
 
-@csrf_exempt
+@twilio
 def remove_from_queue(request):
 
     r = twiml.Response()
 
     r.say('You are being connected to our first available representative.')
 
-    return HttpResponse(str(r))
+    return r
 
-@csrf_exempt
+@twilio
 def agent_remove_from_queue(request):
 
     r = twiml.Response()
     with r.dial() as d:
         d.queue('DefaultQueue', url=resolve_url('remove_from_queue'))
 
-    return HttpResponse(str(r))
+    return r
--- twilio_accelerate_workshop/wrappers.py
+++ twilio_accelerate_workshop/wrappers.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-def twilio(func):
-    def authenticate_and_call(*args, **kwargs):
-        if not Account.is_authentic(request):
-            raise Exception('Authentication Failed.')
-        return func(*args, **kwargs)
-    return authenticate_and_call
+
+def twilio(func, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        response = str(func(*args, **kwargs))
+        return HttpResponse(response)
+    return csrf_exempt(wrapper)

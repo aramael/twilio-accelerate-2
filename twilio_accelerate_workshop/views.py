5f35bec3865ab64469205086a602734d2d71802b
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-07-23 11:07:40 -0400

    Added Fallback

--- twilio_accelerate_workshop/views.py
+++ twilio_accelerate_workshop/views.py
@@ -3,7 +3,10 @@ Django views for twilio_accelerate_workshop project.
 
 """
 
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from twilio import twiml
 
 
 def home(request):
@@ -12,3 +15,19 @@ def home(request):
     context = {}
 
     return render(request, '', context)
+
+@csrf_exempt
+def twilio_fallback(request, fallback_type):
+
+    if fallback_type not in ('voice', 'sms'):
+        return HttpResponseNotFound('Sorry Unable to Fallback')
+
+    print request.POST
+
+    r = twiml.Response()
+    if fallback_type == 'voice':
+        r.say("Our Application is Unavailable at this time, a representative will call you back shortly.")
+    else:
+        r.sms("Our Application is Unavailable at this time, a representative will call you back shortly.")
+
+    return HttpResponse(str(r))

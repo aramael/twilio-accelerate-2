5ffa6206c03a45ba7abedc2cfe34f4ab5fa7d664
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-07-23 09:46:30 -0400

    Added TWILIO Settings

--- settings/staging.py
+++ settings/staging.py
@@ -23,3 +23,22 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # heroku config:add SECRET_KEY=''
 # </code>
 SECRET_KEY = os.environ['SECRET_KEY']
+
+#==============================================================================
+# Twilio Account Settings
+#==============================================================================
+
+# Make this unique, and don't share it with anybody.
+# Set it by issuing following command
+# <code>
+# heroku config:add TWILIO_ACCOUNT=''
+# </code>
+TWILIO_ACCOUNT = os.environ['TWILIO_ACCOUNT']
+
+
+# Make this unique, and don't share it with anybody.
+# Set it by issuing following command
+# <code>
+# heroku config:add TWILIO_TOKEN=''
+# </code>
+TWILIO_TOKEN = os.environ['TWILIO_TOKEN']

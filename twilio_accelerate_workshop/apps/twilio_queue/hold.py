8e9ec9d7849d780fe9f02bfe55e2f188e8c1606d
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-07-23 16:30:58 -0400

    Added Hold Music and Queue Wait

--- twilio_accelerate_workshop/apps/twilio_queue/hold.py
+++ twilio_accelerate_workshop/apps/twilio_queue/hold.py
@@ -29,13 +29,45 @@ class HoldMusic(object):
         'http://com.twilio.music.electronica.s3.amazonaws.com/teru_-_110_Downtempo_Electronic_4.mp3',
     )
     GUITARS = (
-        'http://com.twilio.music.guitars.s3.amazonaws.com/Pitx_-_A_Thought.mp3',
-        'http://com.twilio.music.guitars.s3.amazonaws.com/Pitx_-_Long_Winter.mp3',
+        {
+            'id': '400',
+            'uri': 'http://sms.aramael.com/static/subway.mp3',
+            'time': 34,
+        },
+        {
+            'id': '401',
+            'uri': 'http://sms.aramael.com/static/winters-tale.mp3',
+            'time': 30,
+        },
+        {
+            'id': '402',
+            'uri': 'http://sms.aramael.com/static/get-up.mp3',
+            'time': 31,
+        },
+        {
+            'id': '403',
+            'uri': 'http://com.twilio.music.guitars.s3.amazonaws.com/Pitx_-_A_Thought.mp3',
+            'time': 62,
+        },
+        {
+            'id': '404',
+            'uri': 'http://com.twilio.music.guitars.s3.amazonaws.com/Pitx_-_Long_Winter.mp3',
+            'time': 103
+        }
     )
     ROCK = (
-        'http://com.twilio.music.rock.s3.amazonaws.com/jlbrock44_-_Apologize_Guitar_Deep_Fried.mp3',
-        'http://com.twilio.music.rock.s3.amazonaws.com/jlbrock44_-_Apologize_Guitar_DropC.mp3',
-        'http://com.twilio.music.rock.s3.amazonaws.com/nickleus_-_original_guitar_song_200907251723.mp3',
+        {
+            'id': '500',
+            'uri': 'http://com.twilio.music.rock.s3.amazonaws.com/jlbrock44_-_Apologize_Guitar_Deep_Fried.mp3',
+        },
+        {
+            'id': '501',
+            'uri': 'http://com.twilio.music.rock.s3.amazonaws.com/jlbrock44_-_Apologize_Guitar_DropC.mp3',
+        },
+        {
+            'id': '502',
+            'uri': 'http://com.twilio.music.rock.s3.amazonaws.com/nickleus_-_original_guitar_song_200907251723.mp3',
+        },
     )
     SOFT_ROCK = (
         'http://com.twilio.music.soft-rock.s3.amazonaws.com/_ghost_-_promo_2_sample_pack.mp3',
@@ -45,14 +77,48 @@ class HoldMusic(object):
         'http://com.twilio.music.soft-rock.s3.amazonaws.com/Fireproof_Babies_-_Melancholy_4_a_Sun-lit_day.mp3',
     )
 
+    DEFAULT = "GUITARS"
+
     MUSIC_TYPES = {
-        "1": CLASSICAL,
-        "2": AMBIENT,
-        "3": ELECTRONICA,
-        "4": GUITARS,
-        "5": ROCK,
-        "6": SOFT_ROCK,
+        "1": "CLASSICAL",
+        "2": "AMBIENT",
+        "3": "ELECTRONICA",
+        "4": "GUITARS",
+        "5": "ROCK",
+        "6": "SOFT_ROCK",
     }
 
+    music_choice = None
+
+    def __init__(self, default_music):
+
+        self.DEFAULT = default_music
+        self.time_elapsed = 0
+
     def get_music_type(self, digits):
-        return self.MUSIC_TYPES.get(digits, self.CLASSICAL)
+
+        self.music_choice = digits
+
+        return self.MUSIC_TYPES.get(digits, self.DEFAULT)
+
+    def get_music(self, max_time=None, played=None):
+
+        if played is None:
+            played = []
+
+        music_choice = self.music_choice or self.DEFAULT
+
+        music_type = getattr(self, str(music_choice), None)
+
+        if music_type is None:
+            music_type = getattr(self, self.DEFAULT)
+
+        for music in music_type:
+            if music['id'] not in played:
+                self.time_elapsed += music.get('time', 0)
+
+                if max_time < self.time_elapsed:
+                    return
+
+                yield music
+        return
--- twilio_accelerate_workshop/apps/twilio_queue/views.py
+++ twilio_accelerate_workshop/apps/twilio_queue/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import resolve_url
 from twilio import twiml
+from django.conf import settings
 from .hold import HoldMusic
 from twilio_accelerate_workshop.wrappers import twilio
-from django.conf import settings
-from .shortcuts import redirect_url
+from twilio_accelerate_workshop.shortcuts import redirect_url as url_with_get
 
 
 @twilio
@@ -18,16 +18,17 @@ def add_to_queue(request):
     else:
         queue = 'DefaultQueue'
 
-    wait_url = redirect_url(resolve_url(queue_wait), queue=queue)
+    wait_url = url_with_get(resolve_url(queue_wait), queue=queue)
 
     r.enqueue(queue, waitUrl=wait_url)
 
     return r
 
 @twilio
-def queue_wait(request, digits=None, music_type=HoldMusic.CLASSICAL, hold_music=HoldMusic):
+def queue_wait(request, digits=None, default_music="GUITARS", hold_music=HoldMusic):
 
-    hold_music = hold_music()
+    # Instantiate Hold Music Object
+    hold_music = hold_music(default_music=default_music)
 
     # Set Music URL
     if request.POST and request.POST.get('Digits', None) and digits is None:
@@ -42,9 +43,29 @@ def queue_wait(request, digits=None, music_type=HoldMusic.CLASSICAL, hold_music=
 
     r = twiml.Response()
 
+    queue_position = request.POST.get('QueuePosition', None)
+    queue_wait_time = request.POST.get('AvgQueueTime', None)
+
+    if queue_position and queue_wait_time:
+
+        queue_wait_time = int(queue_wait_time) / 60
+
+        r.say('We appreciate your feedback you are number ' + queue_position + ' in queue. You '
+              'are probably going to wait ' + str(queue_wait_time) + ' minutes')
+
+    # Load Up Play Music
+    played = request.GET.get('played', [])
+
+    if isinstance(played, basestring):
+        played = played.split(',')
+
     with r.gather(numDigits='1', action=resolve_url('queue_wait')) as g:
-        for music in music_type:
-            g.play(music)
+        for music in hold_music.get_music(max_time=40, played=played):
+            played.append(music['id'])
+            g.play(music['uri'])
+
+    # Add Played to Queue List
+    redirect_url = url_with_get(redirect_url, played=played)
 
     r.redirect(redirect_url)
 
